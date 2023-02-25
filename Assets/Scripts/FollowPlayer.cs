169d3e7cf31a03e9a7f65b8b76072809d7cf6731
Heruwim <heruwim25@gmail.com>
2023-02-25 12:19:06 +0200

    CAMERA FOLLOW

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _player;
+    [SerializeField] private Vector3 _offset;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position = _player.position + _offset;
     }
 }
