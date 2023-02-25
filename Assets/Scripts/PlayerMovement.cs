e47e0aab8d3e18d7f5638f474169b6f1b03c08a9
Heruwim <heruwim25@gmail.com>
2023-02-25 11:59:24 +0200

    Cube movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _forwardForce = 2000f;
+    [SerializeField] private float _sidewayForce = 500f;
     
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(0, 0, _forwardForce *  Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            _rigidbody.AddForce(_sidewayForce * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            _rigidbody.AddForce(-_sidewayForce * Time.deltaTime, 0, 0);
+        }
+    }
 }
