266993a86f69401a8ac962b539bf94ad4c77fb95
Heruwim <heruwim25@gmail.com>
2023-02-27 11:41:23 +0200

    Add UI element Score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Text _scoreText;
+
+    private void Update()
+    {
+        _scoreText.text = _player.position.z.ToString("0");
+    }
+}
