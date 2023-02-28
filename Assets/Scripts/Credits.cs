319e40bb3459a9588e8a10f9193979a7aee4bd31
Heruwim <heruwim25@gmail.com>
2023-02-28 18:28:57 +0200

    Add menu

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
